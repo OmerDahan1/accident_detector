8afcf6562aa08848ff009338613253d2d041f043
OmerDahan1 <114734727+OmerDahan1@users.noreply.github.com>
2024-03-19 16:12:27 +0200

    added voice call to the police when ended recognize

--- make_call.py
+++ make_call.py
@@ -5,16 +5,18 @@ key = "e6caa3ae-3e08-4fd7-9ee5-40fa4756e26d"
 secret = os.environ.get("SINCH_SECRET_KEY")
 
 def get_call_text(lisence_plate_number_list):
-  text = f"i am calling from the car accident detection system. We have a car accident at Haifa. The following cars with lisence plate number has detected:"
-  for lisence_plate_number in lisence_plate_number_list:
-    text += f"lisence number  {lisence_plate_number}."
+  text = f"i am calling from the car accident detection system. We have a car accident at Haifa."
+  if (lisence_plate_number_list is not None):
+    text += "The following cars with lisence plate number has detected:"
+    for lisence_plate_number in lisence_plate_number_list:
+      text += f"lisence number  {lisence_plate_number}."
 
   text += "Please hurry up and come here as soon as possible. Thank you."
   return text
 
 def make_call(lisence_plate_number_list):
   from_number = "+447520662233"
-  to = "+9720526012252"
+  to = "+9720543155331"
   locale = "en-US"
   url = "https://calling.api.sinch.com/calling/v1/callouts"
 
